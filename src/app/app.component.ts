0faf8359843038cf0911db5f7e16b35aab628345
Begaret <olofellestad@gmail.com>
2024-12-11 12:17:57 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import {
+  Firestore,
+  collection,
+  collectionData
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent
+{
   title = 'angular-test';
+
+  store: Firestore = inject(Firestore);
+  test: Observable<any[]>;
+
+  constructor()
+  {
+    const coll = collection(this.store, 'test');
+    this.test = collectionData(coll);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+
+import { firebaseConfig } from './environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ]
 };
+
