4d1f993ce3c0664cf40195ad64faf9760f993147
Begaret <olofellestad@gmail.com>
2024-12-13 13:37:33 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from "./footer/footer.component";
+import { MainComponent } from "./main/main.component";
+
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 
@@ -12,20 +16,25 @@ import {
 
 @Component({
   selector: 'app-root',
-  imports: [AsyncPipe],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent
 {
-  title = 'angular-test';
+  title = 'Moodle 1.05';
 
   store: Firestore = inject(Firestore);
-  test: Observable<any[]>;
+  test: Observable<Test[]>;
 
   constructor()
   {
     const coll = collection(this.store, 'test');
-    this.test = collectionData(coll);
+    this.test = collectionData(coll) as Observable<Test[]>;
   }
 }
+
+interface Test
+{
+  test: string;
+};
