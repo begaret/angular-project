a33768fcbac9744fdd0be6bc619505e65703e484
Begaret <olofellestad@gmail.com>
2024-12-18 10:14:52 +0100

    nsdjcnds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
-import { Component, inject } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from "./footer/footer.component";
 import { MainComponent } from "./main/main.component";
-
-import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
-
-import {
-  Firestore,
-  collection,
-  collectionData
-} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -23,18 +14,4 @@ import {
 export class AppComponent
 {
   title = 'Moodle 1.05';
-
-  store: Firestore = inject(Firestore);
-  test: Observable<Test[]>;
-
-  constructor()
-  {
-    const coll = collection(this.store, 'test');
-    this.test = collectionData(coll) as Observable<Test[]>;
-  }
 }
-
-interface Test
-{
-  test: string;
-};
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import {
+  Firestore,
+  collection,
+  collectionData
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  store: Firestore = inject(Firestore);
+  test: Observable<Test[]>;
 
+  constructor()
+  {
+    const coll = collection(this.store, 'test');
+    this.test = collectionData(coll) as Observable<Test[]>;
+  }
 }
+
+interface Test
+{
+  test: string;
+};
