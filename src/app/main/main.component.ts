2171aef0cd70213631230237abe011ea07d80c70
Begaret <olofellestad@gmail.com>
2025-01-17 13:07:57 +0100

    components

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject } from '@angular/core';
+
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import {
+  Firestore,
+  collection,
+  collectionData
+} from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-main',
+  imports: [AsyncPipe],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  store: Firestore = inject(Firestore);
+  courses: Observable<Course[]>;
+
+  constructor()
+  {
+    const data = collection(this.store, 'test');
+    this.courses = collectionData(data) as Observable<Course[]>;
+  }
+}
+
+interface Course
+{
+  code: string;
+  name: string;
+  students: number;
+  grade: string;
+}
+
